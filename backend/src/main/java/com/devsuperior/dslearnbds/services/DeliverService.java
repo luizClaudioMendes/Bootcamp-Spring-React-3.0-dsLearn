9bdee0e2c883937a853d8b1206d16b8a610a24b8
Luiz Mendes <emailluizMendes@gmail.com>
2022-10-12 11:26:59 -0300

    04-26 Pré-autorizando métodos por perfil de usuário

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/DeliverService.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dslearnbds.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dslearnbds.dto.DeliverRevisionDTO;
+import com.devsuperior.dslearnbds.entities.Deliver;
+import com.devsuperior.dslearnbds.repositories.DeliverRepository;
+
+@Service
+public class DeliverService {
+	
+	@Autowired
+	private DeliverRepository repository;
+	
+	@Transactional
+	public void saveRevision( Long id, DeliverRevisionDTO dto) {
+		Deliver entity = repository.getReferenceById(id);
+		
+		entity.setStatus(dto.getStatus());
+		entity.setFeedback(dto.getFeedback());
+		entity.setCorrectCount(dto.getCorrectCount());
+		
+		repository.save(entity);
+	}
+
+}
