3d35f02e7632afc7bf602341b81db2f8b358c96b
Luiz Mendes <emailluizMendes@gmail.com>
2022-10-12 18:08:30 -0300

    ended movieflix task

--- Projeto_MovieFlix/bds05/src/main/java/com/devsuperior/movieflix/services/AuthService.java
+++ Projeto_MovieFlix/bds05/src/main/java/com/devsuperior/movieflix/services/AuthService.java
@@ -1,14 +1,14 @@
-package com.devsuperior.dslearnbds.services;
+package com.devsuperior.movieflix.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.devsuperior.dslearnbds.entities.User;
-import com.devsuperior.dslearnbds.repositories.UserRepository;
-import com.devsuperior.dslearnbds.services.exceptions.ForbiddenException;
-import com.devsuperior.dslearnbds.services.exceptions.UnauthorizedException;
+import com.devsuperior.movieflix.entities.User;
+import com.devsuperior.movieflix.repositories.UserRepository;
+import com.devsuperior.movieflix.services.exceptions.ForbiddenException;
+import com.devsuperior.movieflix.services.exceptions.UnauthorizedException;
 
 @Service
 public class AuthService {
@@ -29,7 +29,7 @@ public class AuthService {
 	public void validateSelfOrAdmin(Long userId) {
 		User user = authenticated();
 		
-		if(!user.getId().equals(userId) && !user.hasRole("ROLE_ADMIN")) {
+		if(!user.getId().equals(userId)) {
 			throw new ForbiddenException("Access Denied");
 		}
 	}
